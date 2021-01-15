3608812269e919e32763ed3203ad0f4444e0cf1c
Piotr Dziekan <pdziekan@igf.fuw.edu.pl>
2021-01-15 13:13:45 +0100

    plotting stuff

--- plotting/plot_series.py
+++ plotting/plot_series.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+plots = ["tau", "nrain", "rmax"]
+
 data = {
         "/mnt/local/pdziekan/wyniki/coalfluctu_dim/ST_Np1e3_nx20_eps1_dt0.1var_Wang/build/" : "1"
         ,"/mnt/local/pdziekan/wyniki/coalfluctu_dim/ST_Np1e3_nx20_eps1_dt0.1var_Wang/build1/" : "2"
@@ -8,16 +10,17 @@ data = {
         ,"/mnt/local/pdziekan/wyniki/coalfluctu_dim/ST_Np1e3_nx20_eps1_dt0.1var_Wang/build3/" : "4"
        }
 
-fig, ax = plt.subplots()
 
-for pre in data:
-  time = np.loadtxt(pre+"time.dat")
-  tau = np.loadtxt(pre+"tau.dat")
-  ax.plot(time, tau, label=data[pre])
-
-plt.xlim([2000, 5000])
-plt.legend()
-
-fig.savefig("/home/pracownicy/pdziekan/tmp/tau.png")
+for plot in plots:
+  fig, ax = plt.subplots()
+  for pre in data:
+    time = np.loadtxt(pre+"time.dat")
+    series = np.loadtxt(pre+plot+".dat")
+    ax.plot(time, series, label=data[pre])
+  
+  plt.xlim([2000, 5000])
+  plt.legend()
+  
+  fig.savefig("/home/pracownicy/pdziekan/tmp/"+plot+".png")
 
 
