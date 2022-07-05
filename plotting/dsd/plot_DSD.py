d42f0877f8d69db619f385d05558b06dcfd737e3
pdziekan <pdziekanp@gmail.com>
2022-07-05 12:37:56 +0200

    plotting updates

--- plotting/dsd/plot_DSD.py
+++ plotting/dsd/plot_DSD.py
@@ -1,6 +1,21 @@
 from plot_DSD_common import plot_DSD, plot_DSD_diff
 import matplotlib.pyplot as plt
 
+labeldict = {
+ 0 : "(a)",
+ 1 : "(b)",
+ 2 : "(c)",
+ 3 : "(d)",
+ 4 : "(e)",
+ 5 : "(f)",
+ 6 : "(g)",
+ 7 : "(h)",
+ 8 : "(i)",
+ 9 : "(j)",
+ 10 : "(k)",
+ 11 : "(l)",
+}
+
 directory_base = "/home/piotr/praca/coal_fluctu_dim/LCM_DSD_fluctuations/data/LCM/Onishi/"
 
 #data_nsd = {}
@@ -35,16 +50,30 @@ i = i+1
 data_color[directory_base + "GA17_Np64e6_nx1_dt0.01_SstpCoal1_Onishi_SdConc1e3Tail_Ens100_T300/"]              = colors[i]
 i = i+1
 data_color[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e1Tail_Ens1e4_T300/"]               = colors[i]
+i = i+1
+data_color[directory_base + "GA17_Np64e6_nx1_dt1_SstpCoal1_Onishi_SdConc1e2Tail_Ens1e4_T300/"]               = colors[i]
 
 
 # nie ma roznic miedzy tymi trzema, dt i sstp_coal nie maja wplywu
 data_labels = {}
 #data_labels[directory_base + "GA17_Np64e6_nx1_dt0.01_SstpCoal1_Onishi_SdConc1e3Tail_Ens100_T300/"] = "DT 0.01 SstpCoal1  Sd1e3Tail"
-data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e3Tail_Ens100_T300/"]  = "dt 0.1  SD 1e3 tail"
-data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal10_Onishi_SdConc1e3Tail_Ens100_T300/"] = "dt 0.01 SD 1e3 tail"
+data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e3Tail_Ens100_T300/"]  = "const SD, $N_\mathrm{SD}=1e3$, large tail init, $\Delta t = 0.1\ \mathrm{s}$"
+data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal10_Onishi_SdConc1e3Tail_Ens100_T300/"] = "const SD, $N_\mathrm{SD}=1e3$, large tail init, $\Delta t = 0.01\ \mathrm{s}$"
+data_labels[directory_base + "GA17_Np64e6_nx1_dt1_SstpCoal1_Onishi_SdConc1e2Tail_Ens1e4_T300/"] = "const SD, $N_\mathrm{SD}=1e2$, large tail init, $\Delta t = 1\ \mathrm{s}$"
+data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e2Tail_Ens1e4_T300/"] = "const SD, $N_\mathrm{SD}=1e2$, large tail init, $\Delta t = 0.1\ \mathrm{s}$"
 
-fig, ax = plt.subplots(1, 2, figsize=(24,9))
-plot_DSD(data_labels, data_color, fig, ax, 300, "timestep")
+fig, ax = plt.subplots(1, 2, figsize=(8,4))
+plot_DSD(data_labels, data_color, False, fig, ax, 300)
+#single legend for the whole figure
+handles, labels = ax[0].get_legend_handles_labels()
+lgd = fig.legend(handles, labels, handlelength=4, loc='lower center', bbox_to_anchor=(0.475,0))
+# a b c d labels
+for i,axs in enumerate(ax.flatten()):
+  axs.text(0.1, 0.9, labeldict[i], fontsize=10, transform=axs.transAxes)
+fig.tight_layout()
+fig.subplots_adjust(bottom=0.26, wspace=0.3)#, hspace=0.25)
+fig.savefig("/home/piotr/praca/coal_fluctu_dim/LCM_DSD_fluctuations/img/DSD_timestep_t300.pdf")
+
 
 
 # porownanie symulacji z i bez losowosci w rozkladzie poczatkowym - nie ma znaczenia, losowosc po 300 s i tak jest taka sama
@@ -53,37 +82,62 @@ data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_ConstMulti1
 data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_ConstMulti1_Ens10_T300_RngSeedInit44/"] = "one-to-one, no randomness in initial DSD"
 #data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e1Tail_Ens1e4_T300/"] =               "DT 0.1  SstpCoal1  Sd1e1Tail"
 #data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e1Tail_Ens100_T300_RngSeedInit44/"] = "DT 0.1  SstpCoal10 Sd1e1Tail RngSeedInit"
-data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e2Tail_Ens1e4_T300/"] =               "SD 1e2 tail"
-data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e2Tail_Ens1e4_T300_RngSeedInit44/"] = "SD 1e2 tail, no randomness in initial DSD"
+data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e2Tail_Ens1e4_T300/"] =               "const SD, $N_\mathrm{SD}=1e2$, large tail init"
+data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e2Tail_Ens1e4_T300_RngSeedInit44/"] = "const SD, $N_\mathrm{SD}=1e2$, large tail init, no randomness in initial DSD"
 
-fig, ax = plt.subplots(1, 2, figsize=(24,9))
-plot_DSD(data_labels, data_color, fig, ax, 300, "initial_randomness")
-fig, ax = plt.subplots(1, 2, figsize=(24,9))
-plot_DSD(data_labels, data_color, fig, ax, 0, "initial_randomness")
+fig, ax = plt.subplots(2, 2, figsize=(8,8))
+plot_DSD(data_labels, data_color, False, fig, ax[0], 0)
+plot_DSD(data_labels, data_color, False, fig, ax[1], 300)
+ax[0,0].set_xlabel('')
+ax[0,1].set_xlabel('')
+ax[0,0].set_title('$t=0\ s$')
+ax[0,1].set_title('$t=0\ s$')
+ax[1,0].set_title('$t=300\ s$')
+ax[1,1].set_title('$t=300\ s$')
+#single legend for the whole figure
+handles, labels = ax[0,0].get_legend_handles_labels()
+lgd = fig.legend(handles, labels, handlelength=4, loc='lower center', bbox_to_anchor=(0.475,0))
+# a b c d labels
+for i,axs in enumerate(ax.flatten()):
+  axs.text(0.1, 0.9, labeldict[i], fontsize=10, transform=axs.transAxes)
+fig.tight_layout()
+fig.subplots_adjust(bottom=0.2, wspace=0.3)#, hspace=0.25)
+fig.savefig("/home/piotr/praca/coal_fluctu_dim/LCM_DSD_fluctuations/img/DSD_initial_randomness_t0_t300.pdf")
 
 
-# porownanie symulacji dla roznej liczby SD
+## porownanie symulacji dla roznej liczby SD
 data_labels = {}
 data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_ConstMulti1_Ens10_T300/"] =     "one-to-one"
-data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e5Tail_Ens100_T300/"] =  "SD 1e5 tail"
-data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e4Tail_Ens100_T300/"] =  "SD 1e4 tail"
-data_labels[directory_base + "GA17_Np64e6_nx1_dt0.01_SstpCoal1_Onishi_SdConc1e3Tail_Ens100_T300/"] = "SD 1e3 tail"
-data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e2Tail_Ens1e4_T300/"] =  "SD 1e2 tail"
-data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e1Tail_Ens1e4_T300/"] =  "SD 1e1 tail"
+data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e5Tail_Ens100_T300/"] =  "const SD, $N_\mathrm{SD}=1e5$, large tail init"
+data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e4Tail_Ens100_T300/"] =  "const SD, $N_\mathrm{SD}=1e4$, large tail init"
+data_labels[directory_base + "GA17_Np64e6_nx1_dt0.01_SstpCoal1_Onishi_SdConc1e3Tail_Ens100_T300/"] = "const SD, $N_\mathrm{SD}=1e3$, large tail init"
+data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e2Tail_Ens1e4_T300/"] =  "const SD, $N_\mathrm{SD}=1e2$, large tail init"
+data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e1Tail_Ens1e4_T300/"] =  "const SD, $N_\mathrm{SD}=1e1$, large tail init"
 
-fig, ax = plt.subplots(1, 2, figsize=(24,9))
-plot_DSD(data_labels, data_color, fig, ax, 300, "NSD")
-fig, ax = plt.subplots(1, 2, figsize=(24,9))
-plot_DSD(data_labels, data_color, fig, ax, 0, "NSD")
+
+fig, ax = plt.subplots(2, 2, figsize=(8,8))
+plot_DSD(data_labels, data_color, True, fig, ax[0], 0)
+plot_DSD(data_labels, data_color, True, fig, ax[1], 300)
+ax[0,0].set_xlabel('')
+ax[0,1].set_xlabel('')
+ax[0,0].set_title('$t=0\ s$')
+ax[0,1].set_title('$t=0\ s$')
+ax[1,0].set_title('$t=300\ s$')
+ax[1,1].set_title('$t=300\ s$')
+#single legend for the whole figure
+handles, labels = ax[0,0].get_legend_handles_labels()
+lgd = fig.legend(handles, labels, handlelength=4, loc='lower center', bbox_to_anchor=(0.475,0))
+# a b c d labels
+for i,axs in enumerate(ax.flatten()):
+  axs.text(0.1, 0.9, labeldict[i], fontsize=10, transform=axs.transAxes)
+fig.tight_layout()
+fig.subplots_adjust(bottom=0.28, wspace=0.3)#, hspace=0.25)
+fig.savefig("/home/piotr/praca/coal_fluctu_dim/LCM_DSD_fluctuations/img/DSD_NSD_t0_t300.pdf")
 
 # roznica SD wzgledem one-to-one
-plt.clf()
 data_labels = {}
 data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_ConstMulti1_Ens10_T300/"] =     "one-to-one"
 data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e5Tail_Ens100_T300/"] =  "SD 1e5 tail"
 data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e2Tail_Ens1e4_T300/"] =  "SD 1e2 tail"
 data_labels[directory_base + "GA17_Np64e6_nx1_dt0.1_SstpCoal1_Onishi_SdConc1e1Tail_Ens1e4_T300/"] =  "SD 1e1 tail"
 plot_DSD_diff(data_labels, data_color, "one-to-one", 300, "NSD")
-
-
-
--- plotting/dsd/plot_DSD_common.py
+++ plotting/dsd/plot_DSD_common.py
@@ -65,7 +65,7 @@ def read_DSD(pre, time):
 
 
 
-def plot_DSD(data_labels, data_colors, fig, ax, time, outname):
+def plot_DSD(data_labels, data_colors, EFM_flag, fig, ax, time):
   for pre in data_labels:
     LCM_r, LCM_m_logr, LCM_m_logr_std_dev, LCM_m_logr_times_vterm, LCM_m_logr_err, vterm_r = read_DSD(pre, time)
     LCM_dlogr = np.log(LCM_r[1]) - np.log(LCM_r[0]) # [log(um)]
@@ -93,42 +93,43 @@ def plot_DSD(data_labels, data_colors, fig, ax, time, outname):
    # ax[1].plot(LCM_r, LCM_m_r_std_dev  * np.sqrt(data_nsd[pre]) / np.sqrt(64e6), label='end ' + data_labels[pre])
   
   # EFM results (Smoluchowski)
-  efm_data = np.genfromtxt("/home/piotr/praca/coal_fluctu_dim/LCM_DSD_fluctuations/data/EFM/dt 0.1_rq015.0_xmw 2.0_scal10.0_isw3_cut****_rmr****_tmax301._boplot00.out", unpack=True)
-  #efm_data = np.genfromtxt("/home/piotr/praca/coal_fluctu_dim/LCM_DSD_fluctuations/data/EFM/dt 0.1_rq015.0_xmw 2.0_scal15.0_isw3_cut****_rmr****_tmax301._boplot00.out", unpack=True)
-  # efm_data[0] - radius[um]
-  # efm_data[1] - mass density m(ln r) [kg/m3] with radius in microns
-  EFM_r = efm_data[0] * 1e-6 # [m]
-  EFM_m_logr = efm_data[1] # [kg/m3 / unit(log[um])]
-  EFM_dlogr = np.log(efm_data[0][1]) - np.log(efm_data[0][0]) # [log(um)]
-  EFM_m_r = EFM_m_logr / EFM_r # mass density m(r) [kg/m3 / m]
-  EFM_M = EFM_m_logr * EFM_dlogr * volume # mass of droplets of this size in the cell [kg]
-  EFM_N = EFM_M / four_over_three_pi_rhow / np.power(EFM_r,3) # number of droplets of this size in the cell [1]
-  EFM_M_std_dev = four_over_three_pi_rhow * np.power(EFM_r,3) * np.sqrt(EFM_N) # std_dev of total mass of droplets of this size in the whole cell [kg]
-  # both give the same
-  EFM_m_logr_std_dev = EFM_M_std_dev / EFM_dlogr / volume # std_dev of mass density m(log r) [kg/m3]
-  #EFM_m_logr_std_dev = np.sqrt(four_over_three_pi_rhow * np.power(EFM_r,3) * EFM_m_logr / EFM_dlogr / volume) # std_dev of mass density m(log r) [kg/m3]
+  if EFM_flag:
+    efm_data = np.genfromtxt("/home/piotr/praca/coal_fluctu_dim/LCM_DSD_fluctuations/data/EFM/dt 0.1_rq015.0_xmw 2.0_scal10.0_isw3_cut****_rmr****_tmax301._boplot00.out", unpack=True)
+    #efm_data = np.genfromtxt("/home/piotr/praca/coal_fluctu_dim/LCM_DSD_fluctuations/data/EFM/dt 0.1_rq015.0_xmw 2.0_scal15.0_isw3_cut****_rmr****_tmax301._boplot00.out", unpack=True)
+    # efm_data[0] - radius[um]
+    # efm_data[1] - mass density m(ln r) [kg/m3] with radius in microns
+    EFM_r = efm_data[0] * 1e-6 # [m]
+    EFM_m_logr = efm_data[1] # [kg/m3 / unit(log[um])]
+    EFM_dlogr = np.log(efm_data[0][1]) - np.log(efm_data[0][0]) # [log(um)]
+    EFM_m_r = EFM_m_logr / EFM_r # mass density m(r) [kg/m3 / m]
+    EFM_M = EFM_m_logr * EFM_dlogr * volume # mass of droplets of this size in the cell [kg]
+    EFM_N = EFM_M / four_over_three_pi_rhow / np.power(EFM_r,3) # number of droplets of this size in the cell [1]
+    EFM_M_std_dev = four_over_three_pi_rhow * np.power(EFM_r,3) * np.sqrt(EFM_N) # std_dev of total mass of droplets of this size in the whole cell [kg]
+    # both give the same
+    EFM_m_logr_std_dev = EFM_M_std_dev / EFM_dlogr / volume # std_dev of mass density m(log r) [kg/m3]
+    #EFM_m_logr_std_dev = np.sqrt(four_over_three_pi_rhow * np.power(EFM_r,3) * EFM_m_logr / EFM_dlogr / volume) # std_dev of mass density m(log r) [kg/m3]
+    
+    # plot m(log r)
+    if time == 0:
+      EFM_m_logr = EFM_m_logr[:int(EFM_N.size/2)]
+      EFM_m_logr_std_dev = EFM_m_logr_std_dev[:int(EFM_N.size/2)]
+    else:
+      EFM_m_logr = EFM_m_logr[int(EFM_N.size/2):]
+      EFM_m_logr_std_dev = EFM_m_logr_std_dev[int(EFM_N.size/2):]
   
-  # plot m(log r)
-  if time == 0:
-    EFM_m_logr = EFM_m_logr[:int(EFM_N.size/2)]
-    EFM_m_logr_std_dev = EFM_m_logr_std_dev[:int(EFM_N.size/2)]
-  else:
-    EFM_m_logr = EFM_m_logr[int(EFM_N.size/2):]
-    EFM_m_logr_std_dev = EFM_m_logr_std_dev[int(EFM_N.size/2):]
-
-  ax[0].plot(EFM_r[:int(EFM_N.size/2)] * 1e6, EFM_m_logr * 1e3, ls='--', color='black', label="SCE") # radius in [um], mass density in [g/m3 / unit(log[um])]
-  if time > 0: # dont plot SCE initial std dev - it is zero...
-    ax[1].plot(EFM_r[:int(EFM_N.size/2)] * 1e6, EFM_m_logr_std_dev * 1e3, ls='--', color='black', label="SCE estimate") # radius in [um], mass density in [g/m3 / unit(log[um])]
-  
-  # plot N (number of droplets in the bin)
-  #ax[0].plot(EFM_r * 1e6, EFM_N, label='end ' + data_labels[pre])
-  
-  #  ax[0].plot(EFM_r * 1e6, efm_data[1] / efm_data[0] * 1e6 * 1e3, label="EFM") 
-  #  ax[1].plot(EFM_r * 1e6, np.sqrt(four_over_three_pi_rhow * np.power(efm_data[0] * 1e-6,3) * EFM_M), label="EFM") # std_dev of total mass of droplets of this size in the whole cell [kg]
-  #  ax[0].plot(efm_data[0], efm_data[1] * EFM_dlogr / four_over_three_pi_rhow / np.power(efm_data[0] * 1e-6,3), label="EFM") # EFM data is mass density m(ln r) [kg/m3] with radius in microns; LCM data is m(r) with radius in meters
-  
-  print("total number of droplets in the cell in EFM at the end: ", np.sum(EFM_N[int(EFM_N.size/2):])) # EFM_N contains init spectrum and final spectrum
-  print("total mass of droplets in the cell in EFM at the end: ", np.sum(EFM_M[int(EFM_M.size/2):]))
+    ax[0].plot(EFM_r[:int(EFM_N.size/2)] * 1e6, EFM_m_logr * 1e3, ls='--', color='black', label="SCE") # radius in [um], mass density in [g/m3 / unit(log[um])]
+    if time > 0: # dont plot SCE initial std dev - it is zero...
+      ax[1].plot(EFM_r[:int(EFM_N.size/2)] * 1e6, EFM_m_logr_std_dev * 1e3, ls='--', color='black', label="SCE") # radius in [um], mass density in [g/m3 / unit(log[um])]
+    
+    # plot N (number of droplets in the bin)
+    #ax[0].plot(EFM_r * 1e6, EFM_N, label='end ' + data_labels[pre])
+    
+    #  ax[0].plot(EFM_r * 1e6, efm_data[1] / efm_data[0] * 1e6 * 1e3, label="EFM") 
+    #  ax[1].plot(EFM_r * 1e6, np.sqrt(four_over_three_pi_rhow * np.power(efm_data[0] * 1e-6,3) * EFM_M), label="EFM") # std_dev of total mass of droplets of this size in the whole cell [kg]
+    #  ax[0].plot(efm_data[0], efm_data[1] * EFM_dlogr / four_over_three_pi_rhow / np.power(efm_data[0] * 1e-6,3), label="EFM") # EFM data is mass density m(ln r) [kg/m3] with radius in microns; LCM data is m(r) with radius in meters
+    
+    print("total number of droplets in the cell in EFM at the end: ", np.sum(EFM_N[int(EFM_N.size/2):])) # EFM_N contains init spectrum and final spectrum
+    print("total mass of droplets in the cell in EFM at the end: ", np.sum(EFM_M[int(EFM_M.size/2):]))
   
   #plt.title('Time at which largest droplet exceeds 3 mm radius and size of the droplet')
   ax[0].set_xlabel('$r\ [\mathrm{\mu m}]$')
@@ -151,14 +152,16 @@ def plot_DSD(data_labels, data_colors, fig, ax, time, outname):
   #ax[0].set_xlim(40,)
   #ax[0].set_ylim(0,0.30)
   
-  plt.grid(axis='y', color='0.5')
-  ax[0].legend()
-  ax[1].legend()
-  fig.savefig("/home/piotr/praca/coal_fluctu_dim/LCM_DSD_fluctuations/img/DSD_"+str(outname)+"_t"+str(time)+".svg")
+#  plt.grid(axis='y', color='0.5')
+#  ax[0].legend()
+#  ax[1].legend()
+#  fig.savefig("/home/piotr/praca/coal_fluctu_dim/LCM_DSD_fluctuations/img/DSD_"+str(outname)+"_t"+str(time)+".pdf")
   #plt.show()
 
 
 def plot_DSD_diff(data_labels, data_colors, ref_label, time, outname):
+  fig, ax = plt.subplots(figsize=(8,8))
+
   key_list = list(data_labels.keys())
   val_list = list(data_labels.values())
   pre_ref = key_list[val_list.index(ref_label)]
@@ -176,16 +179,18 @@ def plot_DSD_diff(data_labels, data_colors, ref_label, time, outname):
     LCM_m_logr_diff_err = np.sqrt(pow(LCM_m_logr_err,2) + pow(LCM_m_logr_err_ref,2))
     LCM_m_logr_times_vterm_diff_err = LCM_m_logr_diff_err * vterm_r # assume vterm has no error 
 #    plt.plot(LCM_r * 1e6, LCM_m_logr_times_vterm - LCM_m_logr_times_vterm_ref, color=data_colors[pre], label= data_labels[pre])
-    plt.errorbar(LCM_r * 1e6, LCM_m_logr_times_vterm - LCM_m_logr_times_vterm_ref, yerr = SE_scale * LCM_m_logr_times_vterm_diff_err, color=data_colors[pre], label= data_labels[pre])
+    ax.errorbar(LCM_r * 1e6, LCM_m_logr_times_vterm - LCM_m_logr_times_vterm_ref, yerr = SE_scale * LCM_m_logr_times_vterm_diff_err, color=data_colors[pre], label= data_labels[pre])
 
 #    plt.plot(LCM_r * 1e6, LCM_m_logr - LCM_m_logr_ref, color=data_colors[pre], label= data_labels[pre])
+  ax.axhline(y=0., color='grey', linestyle='dotted')
+
   
-  plt.xlabel('$r\ [\mathrm{\mu m}]$')
-  plt.ylabel('$(<m>_\mathrm{SD} - <m>_\mathrm{0})\ v_\mathrm{t}\ \ [\mathrm{g/m^3 * m/s \ / \ unit(log[um])}]$')
-  plt.xscale('log')
-#  plt.yscale('symlog', linthresh=1e-3)
-  plt.xlim(7,3e2)
-  plt.ylim()
-  plt.grid(axis='y', color='0.5')
-  plt.legend()
-  plt.savefig("/home/piotr/praca/coal_fluctu_dim/LCM_DSD_fluctuations/img/DSD_diff_"+str(outname)+"_t"+str(time)+".svg")
+  ax.set_xlabel('$r\ [\mathrm{\mu m}]$')
+  ax.set_ylabel('$(<m>_\mathrm{SD} - <m>_\mathrm{0})\ v_\mathrm{t}\ \ [\mathrm{g/m^3 * m/s \ / \ unit(log[um])}]$')
+  ax.set_xscale('log')
+#  ax.set_yscale('symlog', linthresh=1e-3)
+  ax.set_xlim(7,3e2)
+  ax.set_ylim()
+#  ax.grid(axis='y', color='0.5')
+  ax.legend()
+  plt.savefig("/home/piotr/praca/coal_fluctu_dim/LCM_DSD_fluctuations/img/DSD_diff_"+str(outname)+"_t"+str(time)+".pdf")
